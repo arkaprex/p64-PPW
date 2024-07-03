21cd99e5fbf61c49e4c34436fbe3d4101df296da
DaniloMorocho+ <danimorocho188@gmail.com>
2024-07-03 01:54:20 -0500

    actualizado1

---
+++ unidad_01/html/Tarea_eliminar/js/script.js
@@ -0,0 +1,7 @@
+function cargarImagen() {
+    document.getElementById("opcion").classList.remove("hidden");
+}
+
+function eliminarImagen() {
+    document.getElementById("opcion").classList.add("hidden");
+}
