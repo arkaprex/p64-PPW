3214b6c29ad72b06dcd53b6ee16e4389c15f2f7a
DaniloMorocho+ <danimorocho188@gmail.com>
2024-07-03 05:01:53 -0500

    listo

--- unidad_01/html/Deber/js/script.js
+++ unidad_01/html/Deber/js/script.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Página cargada y lista!");
 
+    var div_cajita = document.getElementById('cajita');
+    var id = null;
+    var pos = 0;
+
+    function mover() {
+        clearInterval(id);
+        id = setInterval(frame, 12);
+    }
+
+    function frame() {
+        if (pos === 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            div_cajita.style.left = pos + 'px';
+        }
+    }
+
     var enlaces = document.querySelectorAll('.caja-enlace');
     enlaces.forEach(function(enlace) {
         enlace.addEventListener('click', function(event) {
             event.preventDefault();
+            mover(); // Inicia la animación de mover la 'cajita'
             var url = this.getAttribute('href');
             var confirmar = confirm('Has hecho clic en: ' + this.textContent + '. ¿Deseas cargar esta sección?');
             if (confirmar) {
@@ -12,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
-    
+
     function cargarContenido(url) {
         fetch(url)
             .then(function(response) {
